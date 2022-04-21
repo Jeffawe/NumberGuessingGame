cf7586c9d569d214afa4c70432e2acb19a5c026d
Jeffawe <awagujeffery@gmail.com>
2022-04-22 00:16:31 +0100

    Added Score and Dynamism

--- app.py
+++ app.py
@@ -1,74 +1,154 @@
 from flask import Flask
+from flask import session
 from flask import request
 from flask import render_template
 import random
-
-n = 0
-guess_count = 0
-guess_limit = 5
+from flask_session import Session
 
 app = Flask(__name__)
+app.config['secret_key'] = 'Secret_Key'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_COOKIE_NAME'] = "my_session"
+
+# Create and initialize the Flask-Session object AFTER `app` has been configured
+server_session = Session(app)
+
+
+def save_session(n_save, guess_count_save, guess_limit_save, number_save, tries_save, score_save, button_show_save):
+    session['n'] = n_save
+    session['guess_count'] = guess_count_save
+    session['guess_limit'] = guess_limit_save
+    session['number'] = number_save
+    session['tries'] = tries_save
+    session['score'] = score_save
+    session['button_show'] = button_show_save
+
+
+def score_increase():
+    score = session.get('score')
+
+    score += 1
+    session['score'] = score
+    return score
 
 
 @app.route('/')
 def home():
-    global guess_count
+    session['n'] = 0
+    session['guess_count'] = 0
+    session['guess_limit'] = 5
+    session['tries'] = 5
+    session['score'] = 0
+    session['button_show'] = 0
 
-    guess_count = 0
+    return render_template("index.html")
+
+
+@app.route('/continue')
+def restart():
+    session['n'] = 0
+    session['guess_count'] = 0
+    session['guess_limit'] = 5
+    session['tries'] = 5
+    session['button_show'] = 0
 
     return render_template("index.html")
 
 
 @app.route('/', methods=['POST'])
 def home_post():
-    global guess_count
-    global number
-    global n
+    guess_count_post = session.get('guess_count')
+    guess_limit_post = session.get('guess_limit')
+    tries = session.get('tries')
+    score = session.get('score')
 
-    if guess_count < guess_limit:
+    if guess_count_post < guess_limit_post:
         n = request.form['dan']
-        n = int(n)
-        number = random.randint(1, n)
 
-    return render_template("index2.html", n=n)
+        if n == '':
+            print('Nothing here')
+            msg = 'Add a Value to Continue'
+            return render_template("index.html", msg=msg)
+
+        else:
+            print('Something here')
+            n = int(n)
+            number = random.randint(1, n)
+            session['number'] = number
+            session['n'] = n
+            button_show = 0
+            return render_template("index2.html", n=n, tries=tries, score=score, button_show=button_show)
 
 
 @app.route('/new')
 def my_form():
-    return render_template("index2.html", n=n)
+    n = session.get('n')
+    tries = session.get('tries')
+    score = session.get('score')
+    button_show = session.get('button_show')
+
+    return render_template("index2.html", n=n, tries=tries, score=score, button_show=button_show)
 
 
 @app.route('/new', methods=['GET', 'POST'])
 def my_form_post():
-    global guess_count
-    global guess_limit
+    guess_count = session.get('guess_count')
+    guess_limit = session.get('guess_limit')
+    number = session.get('number')
+    n = session.get('n')
+    tries = session.get('tries')
+    score = session.get('score')
 
     if guess_count <= guess_limit:
         guess = request.form['inputted']
-        guess = int(guess)
 
-        if guess != number:
-            if guess < number:
-                msg = 'The Number is higher'
-                guess_count += 1
-                return render_template("index2.html", msg=msg, n=n)
-
-            if guess > number:
-                msg = 'The Number is lower'
-                guess_count += 1
-                return render_template("index2.html", msg=msg, n=n)
+        if guess == '':
+            print('Nothing here')
+            msg = 'Guess a Value'
+            button_show = 0
+            save_session(n, guess_count, guess_limit, number, tries, score, button_show)
+            return render_template("index2.html", msg=msg, n=n, tries=tries, score=score, button_show=button_show)
 
         else:
-            num = 'You Got it'
-            numb = ''
-            msg = ''
-            return render_template("index2.html", msg=msg, numG=num, numB=numb, n=n)
+            print('Something here')
+            guess = int(guess)
+
+            if guess != number:
+                if guess < number:
+                    msg = 'The Number is higher'
+                    guess_count += 1
+                    tries -= 1
+                    button_show = 0
+                    save_session(n, guess_count, guess_limit, number, tries, score, button_show)
+                    return render_template("index2.html", msg=msg, n=n, tries=tries, score=score,
+                                           button_show=button_show)
+
+                if guess > number:
+                    msg = 'The Number is lower'
+                    guess_count += 1
+                    tries -= 1
+                    button_show = 0
+                    save_session(n, guess_count, guess_limit, number, tries, score, button_show)
+                    return render_template("index2.html", msg=msg, n=n, tries=tries, score=score, button_show=button_show)
+
+            else:
+                num = 'You Got it'
+                numb = ''
+                msg = ''
+                button_show = 1
+                score = score_increase()
+                save_session(n, guess_count, guess_limit, number, tries, score, button_show)
+                return render_template("index2.html", msg=msg, numG=num, numB=numb, n=n, tries=tries, score=score,
+                                       button_show=button_show)
 
     else:
-        num = 'Out of guesses.The number was ' + str(number)
-        numb = ''
+        numb = 'Out of guesses.The number was ' + str(number)
+        numg = ''
         msg = ''
-        return render_template("index2.html", msg=msg, numB=num, n=n, numG=numb)
+        button_show = 2
+        save_session(n, guess_count, guess_limit, number, tries, score, button_show)
+        return render_template("index2.html", msg=msg, numB=numb, n=n, numG=numg, tries=tries, score=score,
+                               button_show=button_show)
 
 
 if __name__ == '__main__':
